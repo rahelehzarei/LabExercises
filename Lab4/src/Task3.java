99d58358c33105c244d21df5ad3df1006afc6bc8
unknown <raheleh.zarei@gmail.com>
2018-02-13 13:13:45 -0800

    // Query 10 - Find city that has largest number of lowercase characters

--- Lab4/src/Task3.java
+++ Lab4/src/Task3.java
@@ -109,6 +109,14 @@ public class Task3{
         String city = tradersList.stream().map(t -> t.getCity()).max((city1, city2) -> (int)(getLowercaseCharsCount(city1) - getLowercaseCharsCount(city2))).orElse("No city found");
         System.out.println("Query 9:");
         System.out.println(city);
+
+        // Query 10 - Find city that has largest number of lowercase characters
+        Optional<String> CitiesLNoLowC = transactions.stream()
+                .max(Comparator.comparingLong(f -> f.getTrader().getCity().chars().mapToObj(i -> (char) i)
+                        .filter(Character::isLowerCase).count()))
+                .map(transaction -> transaction.getTrader().getCity());
+        System.out.println("city String with the largest number of lowercase letters from all the cities in the transaction list");
+        System.out.println(CitiesLNoLowC);
         
     }
     
